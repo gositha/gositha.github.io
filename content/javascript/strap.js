f0c526a2a498ebf37cc110a7e720629ac9ac1798
Gositha <73519667+gositha@users.noreply.github.com>
2020-10-30 11:09:44 +0000

    Create strap.js

---
+++ content/javascript/strap.js
@@ -0,0 +1,8 @@
+ // Copyright Gositha R 2020. All Rights Reserved.
+ // You may use this javascript for your personal use, but it cannot be sold.
+ 
+ function loadPage() {
+ document.head.innerHTML = document.head.innerHTML + '<link href="/content/images/icon.jpg" rel="icon" />';
+ document.head.innerHTML = document.head.innerHTML + '<link href="/content/themes/404.css" rel="stylesheet">';
+ }
+loadPage()
